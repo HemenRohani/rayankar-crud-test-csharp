bfb379ed3613c475f3af315ee445e9cc0f8a46b2
hemenrohani <hemen.rohani@gmail.com>
2023-05-16 18:12:37 +0330

    Add App Initializer with Extentions

---
+++ src/Mc2.CrudTest.Infrastructure/EF/Options/DataBaseOptions.cs
@@ -0,0 +1,5 @@
+﻿namespace Mc2.CrudTest.Infrastructure.EF.Options;
+public class DataBaseOptions
+{
+    public string ConnectionString { get; set; }
+}
---
+++ src/Mc2.CrudTest.Infrastructure/EF/Services/TravelerCheckListReadService.cs
@@ -0,0 +1,17 @@
+﻿using Mc2.CrudTest.Application.Services;
+using Mc2.CrudTest.Infrastructure.EF.Contexts;
+using Mc2.CrudTest.Infrastructure.EF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mc2.CrudTest.Infrastructure.EF.Services;
+internal sealed class CustomerReadService : ICustomerReadService
+{
+    private readonly DbSet<CustomerReadModel> _Customer;
+
+    public CustomerReadService(ReadDbContext context)
+        => _Customer = context.Customer;
+
+    public Task<bool> ExistsAsync(string firstname, string lastname, DateOnly dateOfBirth)
+        => _Customer.AnyAsync(pl => pl.Firstname == firstname && pl.Lastname == lastname && pl.DateOfBirth == dateOfBirth);
+
+}
